a4fd9bcfa305e74d84f24206c8ed4450021edad6
Tymoteusz Kiepura <kiepurek@gmail.com>
2022-05-19 20:02:01 +0200

    React router integrated and styles for button-like links fixed.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 import {Header} from "./components/layout/Header";
 import {Map} from "./components/Map/Map";
 import { SearchContext } from './contexts/search.context';
+import {AddForm} from "./components/AddForm/AddForm";
+import {Routes,Route} from 'react-router-dom';
 
 export const App = () => {
     const [search,setSearch] = useState('');
@@ -11,7 +13,10 @@ export const App = () => {
 
           <SearchContext.Provider value={{search,setSearch}}>
               <Header/>
-              <Map/>
+              <Routes>
+                  <Route path="/" element={<Map/>}/>
+                  <Route path="/add" element={<AddForm/>}/>
+              </Routes>
           </SearchContext.Provider>
           {/*// searchProvider decyduje nam co mamy w kontekście (tak jakby stan globalny*/}
       </>
